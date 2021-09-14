2391cdee8517a9129eec7ebb233ac4418a8a7f3e
Hugo de Melo <hsemog@gmail.com>
2021-09-14 19:53:03 +0200

    core(swagger): add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,12 +7,23 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const configureSwagger = app => {
+  const options = new DocumentBuilder()
+    .setTitle('Angular with Nx')
+    .setDescription('REST API for the Angular with Nx application')
+    .setVersion('1.0')
+    .build();
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, options));
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.enableCors();
   app.setGlobalPrefix(globalPrefix);
+  configureSwagger(app);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
