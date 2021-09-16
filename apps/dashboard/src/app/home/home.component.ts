2703e48cbf26d89291bece9ba4d791a290961ba9
Hugo de Melo <hsemog@gmail.com>
2021-09-16 20:19:36 +0200

    bugfix: use facade instead of service

--- apps/dashboard/src/app/home/home.component.ts
+++ apps/dashboard/src/app/home/home.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Widget } from '@angular-nx/api-interfaces';
 import { Observable } from 'rxjs';
-import { WidgetsService } from '@angular-nx/core-data';
+import { WidgetsFacade } from '@angular-nx/core-state';
 
 @Component({
   selector: 'angular-nx-home',
   templateUrl: './home.component.html',
   styleUrls: [ './home.component.scss' ]
 })
-export class HomeComponent {
-  widgets$: Observable<Widget[]> = this.widgetService.all();
+export class HomeComponent implements OnInit {
+  allWidgets$: Observable<Widget[]> = this.widgetsFacade.allWidgets$;
 
-  constructor(private readonly widgetService: WidgetsService) {
+  constructor(private readonly widgetsFacade: WidgetsFacade) {
+  }
+
+  ngOnInit(): void {
+    this.widgetsFacade.loadWidgets();
   }
 }
